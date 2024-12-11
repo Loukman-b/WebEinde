995afc2def197a9ed72037e404b6cf3075c9e9d9
Loukman Bouhtala <B.loukman2004@gmail.com>
2024-12-11 10:15:37 +0100

    malfunctions.index is nu de vaste homepage. De rest van de routes zijn aangemaakt en de store en create zijn nu alleen toegankelijk voor ingelogde gebruikers

--- routes/web.php
+++ routes/web.php
@@ -15,17 +15,18 @@ use App\Http\Controllers\MalfunctionsController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',
+[MalfunctionsController::class, 'index'])
+->name('malfunctions.index');
 
-Route::get('/malfunction', [MalfunctionsController::class, 'index'])->name('malfunction.index');
-Route::post('/malfunction', [MalfunctionsController::class, 'store'])->name('malfunction.store');
-Route::get('/malfunction/create', [MalfunctionsController::class, 'create'])->name('malfunction.create');
-Route::get('/malfunction/{malfunction}', [MalfunctionsController::class, 'show'])->name('malfunction.show');
-Route::get('/malfunction/{malfunction}/edit', [MalfunctionsController::class, 'edit'])->name('malfunction.edit');
-Route::put('/malfunction/{malfunction}', [MalfunctionsController::class, 'update'])->name('malfunction.update');
-Route::delete('/malfunction/{malfunction}', [MalfunctionsController::class, 'destroy'])->name('malfunction.destroy');
+
+Route::get('/malfunction', [MalfunctionsController::class, 'index'])->name('malfunctions.index');
+// de middelware functie zorgt ervoor dat de pagina alleen toegankelijk is voor ingelogde gebruikers
+Route::post('/malfunction', [MalfunctionsController::class, 'store'])->middleware('auth')->name('malfunctions.store'); 
+Route::get('/malfunction/create', [MalfunctionsController::class, 'create'])->middleware('auth')->name('malfunctions.create');
+Route::get('/malfunction/{malfunction}/edit', [MalfunctionsController::class, 'edit'])->name('malfunctions.edit');
+Route::put('/malfunction/{malfunction}', [MalfunctionsController::class, 'update'])->name('malfunctions.update');
+Route::delete('/malfunction/{malfunction}', [MalfunctionsController::class, 'destroy'])->name('malfunctions.destroy');
 
 
 Auth::routes();
